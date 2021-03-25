850de5f1c1be54b55c39feeb545e829a97185a18
tr3yd4n <80318628+tr3yd4n@users.noreply.github.com>
2021-03-25 17:03:00 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-let listOfNames = []
+
+window.onload();{
+    let listOfNames = []
+    const nameInput = document.querySelector("#name")
+    const addButton = document.querySelector("#addButton")
+    addButton.addEventListener("click", function(){
+
+        const value = nameInput.value
+        const cleanedValue = value.trim()
+        if (cleanedValue.length !==0){
+            const listedName = document.createElement("li")
+            listedName.className = "inputed-Name"
+            listedName.innerText = cleanedValue
+            listedName.appendChild(listedName)
+        }
+    })
+}    
 
 
-function addNameToArray(){
-    let submitName = document.getElementById("name")
-    listOfNames.push(submitName)
-    
-}
+
+
+{/* <label for="fname">Enter name</label><br>
+    <input type="text" id="name" name="name"><br>
+    <button><a href="" onclick="addNameToArray()">Submit</a></button><br></br> */}
+
+
 
 const counter = document.getElementById("clickme"),
     count = 0
@@ -13,3 +31,5 @@ const counter = document.getElementById("clickme"),
     count += 1
     counter.innerHTML = count
     }
+
+
